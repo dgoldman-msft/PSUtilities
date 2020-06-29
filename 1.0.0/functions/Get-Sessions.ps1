bc0f4918897c1467ec162a8b2e9efb02ecd21569
Dgoldman Microsoft <39909564+dgoldman-msft@users.noreply.github.com>
2020-06-29 15:11:57 -0400

    Initial commit

---
+++ 1.0.0/functions/Get-Sessions.ps1
@@ -0,0 +1,64 @@
+﻿function Get-Sessions
+{
+	<#
+		.Synopsis
+			Custom PowerShell session reporter
+
+		.Description
+			This is a wrapper function for Get-PSSession
+
+		.Example
+			Get-Sessions
+
+		.Notes
+            NOTE: This output uses a custom ps1xml file PowerShellUtilities.Sessions
+	#>
+
+	[CmdletBinding(DefaultParameterSetName = "Default")]
+	param()
+
+	begin {}
+
+	process {
+		Write-PSFMessage -Level Verbose  -Message "Retreiving Sessions"
+		try
+		{
+			foreach($session in Get-PSSession)
+			{
+				if($session.Name -like 'ExchangeOnline*'){[string]$Name = 'Exo'}
+				else {[string]$Name = $session.Name}
+
+				if($session.psbase.ApplicationPrivateData.ContainsKey('SupportedVersions'))
+				{[string]$SupportedVersions = ($session.psbase).ApplicationPrivateData.SupportedVersions.ToString()
+				}else{ [string]$SupportedVersions = 'Not Available'}
+
+				[PSCustomObject]@{
+					PSTypeName            = 'PSUtilities.Sessions'
+					Id                    = $session.Id
+					Name                  = $Name
+					'Computer Name'       = $session.ComputerName
+					Type         		  = $session.ComputerType
+					'Session State'       = $session.State
+					'Versions Supported'  = $SupportedVersions
+					'Server Config'       = $session.ConfigurationName
+					'Is Availabile'       = $session.Availability
+					# Connection Information
+					AppName 			  = $session.Runspace.ConnectionInfo.AppName
+					ConnectionURI		 = $session.Runspace.ConnectionInfo.ConnectionURI
+					ProxyAuthentication	  = $session.Runspace.ConnectionInfo.ProxyAuthentication
+					Port				  = $session.Runspace.ConnectionInfo.Port
+					UseCompression		  = $session.Runspace.ConnectionInfo.UseCompression
+					# Timeout Information
+					'CancelTimeout - In minutes' = (New-Timespan -seconds ($session.Runspace.ConnectionInfo.CancelTimeout/1000)).TotalMinutes
+					'IdleTimeout - In Minutes' = (New-Timespan -seconds ($session.Runspace.ConnectionInfo.IdleTimeout/1000)).TotalMinutes
+					'OpenTimeout - In Minutes' = (New-Timespan -seconds ($session.Runspace.ConnectionInfo.OpenTimeout/1000)).TotalMinutes
+					'OperationTimeout - In Minutes' = (New-Timespan -seconds ($session.Runspace.ConnectionInfo.OperationTimeout/1000)).TotalMinutes}
+				}
+		}
+		catch
+		{
+			Stop-PSFFunction -Message "ERROR: Get-Session failure" -Cmdlet $PSCmdlet -ErrorRecord $_
+		}
+	}
+	end{}
+}
---
+++ 1.0.0/functions/Get-jobTypes.ps1
@@ -0,0 +1,58 @@
+﻿function Get-JobTypes
+{
+	<#
+		.Synopsis
+			Custom PowerShell Job reporter
+
+		.Description
+			This is a wrapper function for Get-Job and Receive-Job with a custom display
+
+		.Example
+			Get-JobTypes | Format-Table
+
+		.Example
+			GJT | Format-Table
+
+		.Notes
+			NOTE: This output uses a custom ps1xml file PowerShellUtilities.Job
+			https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_format.ps1xml?view=powershell-7
+	#>
+
+	[Alias('gjt')]
+	[CmdletBinding(DefaultParameterSetName = "Default")]
+	param()
+
+	process
+	{
+		try
+		{
+			$index = 0
+			foreach ($job in (Get-Job))
+			{
+				# Store the job so we can display it with a custom display - 16 properties
+				[PSCustomObject]@{
+					PSTypeName = 'PSUtilities.Jobs'
+					Index = $index
+					Name = $job.Name
+					JobId = $job.id
+					'Job Start Time' = $job.PSBeginTime
+					'Job End Time' = $job.PSEndTime
+					'Child Job Id(s)' = $job.ChildJobs
+					TypeOfJob = $job.PSJobTypeName
+					JobState = $job.State
+					ContainsData = $job.HasMoreData
+					Executed = $job.Location
+					Command = $job.Command
+					Data = (Receive-Job -Name $job.Name -Keep)
+					'Status Message' = $job.StatusMessage
+					'Job Warning' = $job.Warning
+					'Job Error' = $job.Error}
+				$index++
+			}
+		}
+		catch
+		{
+			Stop-PSFFunction -Message "ERROR: Get-Job failure" -Cmdlet $PSCmdlet -ErrorRecord $_
+        }
+	}
+}
